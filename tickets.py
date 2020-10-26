bab4e4dcc3d9f18c6d30bdd575e0c4e2bb9d5e71
savelii-qa <73199583+savelii-qa@users.noreply.github.com>
2020-10-26 13:48:49 +0300

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -1,14 +1,3 @@
-#def luckyTickets(num):
- #   if num % 2 != 0 or num <= 0: return '# Error: invalid number'
-  #  array = [1] * 10 + [0] * (num // 2 * 9 - 9)
- #   for i in range(num // 2 - 1):
- #       array = [sum(array[x::-1]) if x < 10 else sum(array[x:x - 10:-1]) for x in range(len(array))]
- #   return sum([x ** 2 for x in array])
-
-#print(luckyTickets(6))
-
-
-
 start = 0     # первое значение
 finish = 999999    # последнее значение
 cnt = 0  # счетчик
